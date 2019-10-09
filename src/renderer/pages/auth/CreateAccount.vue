5eb97329062a9d7c8d905a58a505371891beaa6e
Sangmin Kim <plantstoen@gmail.com>
2019-10-09 16:57:13 +0900

    mod: redisgned Lovin.vue

--- src/renderer/pages/auth/CreateAccount.vue
+++ src/renderer/pages/auth/CreateAccount.vue
@@ -1,18 +1,37 @@
 <template>
 <div>
+<div id="main">
   <span @click="$router.push({name: 'login_page'})" id="backward">뒤로가기</span>
   <div class="inputform">
     <input type="text" name="ID" placeholder="Id를 입력해주세요" class="inputform__inputbox" v-model="id">
     <input type="password" name="ID" placeholder="비밀번호를 입력해주세요" class="inputform__inputbox" v-model="pw">
     <input type="password" name="ID" placeholder="비밀번호를 다시 입력해주세요" class="inputform__inputbox" v-model="pw">
   </div>
+  <span>가입시 필요한 이메일 인증입니다</span>
+    <div class="emailconfirm">
+      <input type="text" name="ID" placeholder="이메일을 입력해주세요" class="emailconfirm__inputbox" v-model="id">
+      <button type="button" class="emailconfirm__sendbutton">send</button>
+    </div>
   <button @click="$router.push({name: 'select_map'})">내 건물 찾기</button>
+</div>
 </div>    
 </template>
 
 <style lang="scss" scoped>
+@import url(https://cdn.jsdelivr.net/gh/moonspam/NanumSquare@1.0/nanumsquare.css);
+
+#main{
+  width: 30%;
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  margin: auto;
+}
 #backward{
   font-weight: 700;
+  font-family: 'NanumSquare', sans-serif;
   border: none;
   background-color: white;
   color: gray;
@@ -23,18 +42,63 @@
   margin-left: 1.9em;
 }
 .inputform{
+  width: 100%;
+  margin-top: 3em;
+  margin-bottom: 3em;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   &__inputbox {
-    margin-top: 0.2em;
-    margin-bottom: 0.2em;
+    width: 100%;
     height: 3em;
     border-radius: 1em;
     border: 0.1em solid rgb(189, 186, 186);
     outline-style: none;
     padding-left: 1em;
+    margin-top: 0.3em;
+    margin-bottom: 0.3em;
+    &:focus{
+      border: 0.2em solid rgba(12, 202, 69, 0.295);
+    }
+  }
+  &__sendbutton{
+    border: 0.1em solid white;
+    border-radius: 1em;
+    font-weight: bold;
+    color: gray;
+    width: 2em;
+    height: 3em;
+    outline-style: none;
+    &:hover{
+      background-color: rgba(142, 234, 170, 0.575);
+      color: gray;
+      font-weight: bold;
+    }
+    &:focus{
+      background-color: rgba(142, 234, 170, 0.575);
+      color: gray;
+      font-weight: bold;  
+    }
+  }
+}
+.emailconfirm{
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &__inputbox {
+    width: 100%;
+    height: 3em;
+    border-radius: 1em;
+    border: 0.1em solid rgb(189, 186, 186);
+    outline-style: none;
+    padding-left: 1em;
+    margin-top: 0.3em;
+    margin-bottom: 0.3em;
+    &:focus{
+      border: 0.2em solid rgba(12, 202, 69, 0.295);
+    }
   }
 }
 </style>
--- src/renderer/pages/auth/Login.vue
+++ src/renderer/pages/auth/Login.vue
@@ -23,14 +23,10 @@
     <div class="top">
       <img src="@/assets/Wand_logo.png" id='applogo'>
       <div class="loginbox">
-        <img src="@/assets/Wand_textlogo.png" width="200em" id="textlogo">
+        <!--img src="@/assets/Wand_textlogo.png" width="200em" id="textlogo"-->
         <input type="text" name="ID" placeholder="id" class="loginbox__inputbox" v-model="id">
         <input type="password" name="ID" placeholder="password" class="loginbox__inputbox" v-model="pw">
         <button type="button" class="loginbox__button" @click="login_funtion(id, pw)">로그인</button>
-        <div class="account">
-          <span class="loginbox__text">등록된 건물이 없나요?</span>
-          <span @click="$router.push({name: 'create_account'})" id="create_msg">회원가입하기</span>
-        </div>
       </div>
     </div>
   </div>
@@ -41,6 +37,7 @@
   display: flex;
   flex-direction: row;
   justify-content: center;
+  align-items: center;
   margin-top: 10em;
 }
 .loginbox{
@@ -55,6 +52,10 @@
     border: 0.1em solid rgb(189, 186, 186);
     outline-style: none;
     padding-left: 1em;
+    &:focus{
+      border: 0.2em solid rgba(12, 202, 69, 0.295);
+      font-weight: 600;
+    }
   }
   &__button{
     border: 0.1em solid white;
@@ -69,6 +70,11 @@
       color: gray;
       font-weight: bold;
     }
+    &:focus{
+      background-color: rgba(142, 234, 170, 0.575);
+      color: gray;
+      font-weight: bold;  
+    }
     //tap 키로 선택했을떄도 색 변경해주기
   }
 }
@@ -82,7 +88,7 @@
   color: gray;
   &:hover {
     cursor: pointer;
-    color: rgb(44, 218, 96);
+    color: rgba(29, 216, 85, 0.856);
   }
   margin-left: 1.9em;
 }
